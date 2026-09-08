a95863c05e6ad8ffd31e67914e25d0d80b2014db
Abhaypraja09 <abhaypraja09@gmail.com>
2026-09-08 14:59:25 +0530

    feat: add axios 401 auto-redirect interceptor and update dist build

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 import App from './App.jsx'
 import './index.css'
+
+// Global Axios Interceptors
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem('sa_token')
+  if (token && !config.headers.Authorization) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+// Global 401 interceptor to clear stale token and redirect to login
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('sa_token')
+      localStorage.removeItem('sa_user')
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login'
+      }
+    }
+    return Promise.reject(error)
+  }
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
